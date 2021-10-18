350a3e9739205fa100ea7cd460609be0abedd0c2
Jacob Carpenter <jakecarpenter407@gmail.com>
2021-10-18 13:52:09 -0600

    added wombat

--- app/controllers/ZooController.js
+++ app/controllers/ZooController.js
@@ -1,9 +1,16 @@
 //animal imports
 import { Tiger } from "../models/Tiger.js"
+import { Wombat } from "../models/Wombat.js"
 
 //private
 let _TigerCage = {
-    steve: new Tiger('steve', 20)
+    steve: new Tiger('steve', 20),
+    kevin: new Tiger('kevin', 10),
+    lucy: new Tiger('lucy', 24)
+}
+let _WombatCage = {
+    shmooby = new Wombat('shmooby', 2),
+    schlooby = new Wombat('schlooby', 3)
 }
 
 
---
+++ app/models/Wombat.js
@@ -0,0 +1,9 @@
+//private
+
+//public
+export class Wombat {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+}
